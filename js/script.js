12074aaf1d2f63058f31819f2252cc37628e4932
osmarmcn <osmarn84@gmail.com>
2023-12-31 13:00:08 -0300

    concluindo header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".dynamic-text", {
+    strings: ["Full Stack Developer", "freelancer", "student"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
