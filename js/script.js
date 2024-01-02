c426555bc059d2be656329c37f1a6bf08e7a0598
osmarmcn <osmarn84@gmail.com>
2024-01-02 20:14:56 -0300

    ajuste

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ var typed = new Typed(".dynamic-text", {
     backDelay:1000,
     loop: true
 })
+
+// -------------------------------------------------------------------------------//
+
+let section = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () =>{
+    section.forEach(sec =>{
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+// ----------------------------------------------------------------------------------//
+
+let header = document.querySelector('header')
+header.classList.toggle('sticky', window.scrollY > 100)
